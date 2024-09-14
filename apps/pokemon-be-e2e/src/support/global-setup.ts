af78813360df9f5215ee355f2265901bed929926
munirapp <softwaremakassar@gmail.com>
2024-09-14 16:00:16 +0700

    test: enhance global setup and teardown pokemon-be-e2e

--- apps/pokemon-be-e2e/src/support/global-setup.ts
+++ apps/pokemon-be-e2e/src/support/global-setup.ts
@@ -1,3 +1,5 @@
+import { spawn } from 'child_process';
+
 /* eslint-disable */
 var __TEARDOWN_MESSAGE__: string;
 
@@ -5,6 +7,15 @@ module.exports = async function () {
   // Start services that that the app needs to run (e.g. database, docker-compose, etc.).
   console.log('\nSetting up...\n');
 
+  const server = spawn('npx', ['nx', 'run', 'pokemon-be:serve'], {
+    shell: true,
+    stdio: 'inherit',
+  });
+
+  globalThis.__SERVER_PROCESS__ = server;
+
   // Hint: Use `globalThis` to pass variables to global teardown.
   globalThis.__TEARDOWN_MESSAGE__ = '\nTearing down...\n';
+
+  await new Promise((resolve) => setTimeout(resolve, 5000));
 };
