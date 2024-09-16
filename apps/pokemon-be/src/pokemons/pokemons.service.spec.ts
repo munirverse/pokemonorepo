bb6a392a31ee1edcad20efa9aa0c8515c2b588cd
munirapp <softwaremakassar@gmail.com>
2024-09-16 08:37:10 +0700

    feat: add pokemon service

---
+++ apps/pokemon-be/src/pokemons/pokemons.service.spec.ts
@@ -0,0 +1,92 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { PokemonsService, SIMPLE_POKEMON_DATA } from './pokemons.service';
+import { MockFunctionMetadata, ModuleMocker } from 'jest-mock';
+import { PokemonsRepository } from './pokemons.repository';
+
+const moduleMocker = new ModuleMocker(global);
+
+describe('PokemonsService', () => {
+  let service: PokemonsService;
+  let repository: PokemonsRepository;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [PokemonsService],
+    })
+      .useMocker((token) => {
+        const mockData = [
+          {
+            id: 25,
+            name: 'pikachu',
+            types: [
+              {
+                types: {
+                  name: 'electric',
+                },
+              },
+            ],
+            species: {
+              color: 'yellow',
+              shape: 'quadruped',
+            },
+            sprites: [
+              {
+                category: 'home',
+                viewAngle: 'front_default',
+                url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/25.png',
+              },
+            ],
+          },
+        ];
+
+        if (token === PokemonsRepository) {
+          return { find: jest.fn().mockResolvedValue(mockData) };
+        }
+
+        if (typeof token === 'function') {
+          const mockMetadata = moduleMocker.getMetadata(
+            token
+          ) as MockFunctionMetadata<any, any>;
+
+          const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+
+          return new Mock();
+        }
+      })
+      .compile();
+
+    service = module.get<PokemonsService>(PokemonsService);
+
+    repository = module.get<PokemonsRepository>(PokemonsRepository);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should called expected function and return expected result', async () => {
+    // Given
+    const params = {};
+
+    const expectedResult: SIMPLE_POKEMON_DATA[] = [
+      {
+        id: 25,
+        name: 'pikachu',
+        types: ['electric'],
+        color: 'yellow',
+        shape: 'quadruped',
+        icon: [
+          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/25.png',
+        ],
+      },
+    ];
+
+    // When
+    const data = await service.getPokemons(params);
+
+    // Then
+    expect(repository.find).toHaveBeenCalled();
+
+    expect(data).toMatchObject(expectedResult);
+  });
+});
---
+++ apps/pokemon-be/src/pokemons/pokemons.service.ts
@@ -0,0 +1,72 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { PokemonFindParams, PokemonsRepository } from './pokemons.repository';
+import { Prisma } from '@prisma/client';
+
+const POKEMON_CATEGORY = {
+  DEFAULT: 'default',
+  OFFICIAL_ARTWORK: 'official-artwork',
+  DREAM_WORLD: 'dream_world',
+  HOME: 'home',
+  SHOWDOWN: 'showdown',
+} as const;
+
+const POKEMON_VIEW_ANGLE = {
+  FRONT_DEFAULT: 'front_default',
+  BACK_FEMALE: 'back_female',
+  BACK_SHINY_FEMALE: 'back_shiny_female',
+  FRONT_SHINY: 'front_shiny',
+  BACK_DEFAULT: 'back_default',
+  FRONT_SHINY_FEMALE: 'front_shiny_female',
+  FRONT_FEMALE: 'front_female',
+  BACK_SHINY: 'back_shiny',
+} as const;
+
+export type SIMPLE_POKEMON_DATA = {
+  id: number;
+  name: string;
+  types: string[];
+  color: string;
+  shape: string;
+  icon: string[];
+};
+
+@Injectable()
+export class PokemonsService {
+  constructor(private repository: PokemonsRepository) {}
+
+  async getPokemons(params: PokemonFindParams) {
+    try {
+      const rawPokemons = (await this.repository.find(
+        params
+      )) as Prisma.PokemonGetPayload<{
+        include: {
+          sprites: true;
+          species: true;
+          types: { include: { types: true } };
+        };
+      }>[];
+
+      const pokemons = rawPokemons.map<SIMPLE_POKEMON_DATA>((pokemon) => ({
+        id: pokemon.id,
+        name: pokemon.name,
+        types: pokemon.types.map((typeList) => typeList.types.name),
+        color: pokemon.species.color,
+        shape: pokemon.species.shape,
+        icon: pokemon.sprites
+          .filter(
+            (sprite) =>
+              sprite.category === POKEMON_CATEGORY.HOME &&
+              sprite.viewAngle === POKEMON_VIEW_ANGLE.FRONT_DEFAULT
+          )
+          .map((sprite) => sprite.url),
+      }));
+
+      return pokemons;
+    } catch (error) {
+      throw new InternalServerErrorException('failed to get pokemons', {
+        cause: error,
+        description: error.message,
+      });
+    }
+  }
+}
