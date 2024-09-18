c74502a39d61cd246932f3ceb47f945de7a48404
munirapp <softwaremakassar@gmail.com>
2024-09-18 19:09:35 +0700

    test: add get pokemons e2e negative flow test

---
+++ apps/pokemon-be-e2e/src/pokemon-be/fixtures/get-pokemon/negative-flow/scenario7.ts
@@ -0,0 +1,10 @@
+export default {
+  message: [
+    'name must be longer than or equal to 1 characters',
+    'types must be longer than or equal to 3 characters',
+    'color must be longer than or equal to 3 characters',
+    'shape must be longer than or equal to 3 characters',
+  ],
+  error: 'Bad Request',
+  statusCode: 400,
+};
--- apps/pokemon-be-e2e/src/pokemon-be/get-pokemons.spec.ts
+++ apps/pokemon-be-e2e/src/pokemon-be/get-pokemons.spec.ts
@@ -1,8 +1,10 @@
+import qs from 'querystring';
 import axios from 'axios';
 import expectedObjectScenario1 from './fixtures/get-pokemon/happy-flow/scenario1';
 import expectedObjectScenario2 from './fixtures/get-pokemon/happy-flow/scenario2';
 import expectedObjectScenario3 from './fixtures/get-pokemon/happy-flow/scenario3';
 import expectedObjectScenario4 from './fixtures/get-pokemon/happy-flow/scenario4';
+import expectedObjectScenario7 from './fixtures/get-pokemon/negative-flow/scenario7';
 
 describe('GET /api/pokemons', () => {
   describe('happy flow scenario', () => {
@@ -93,6 +95,67 @@ describe('GET /api/pokemons', () => {
       expect(res.status).toBe(200);
 
       expect(res.data).toMatchObject(expectedResponse);
+    });
+  });
+
+  describe('negative flow scenario', () => {
+    it('scenario5: should return expected result when invalid pagination params init', async () => {
+      // Given
+      const expectedResponse = 'Request failed with status code 400';
+
+      // When & Then
+      await expect(axios.get(`/api/pokemons?page=NaN`)).rejects.toThrow(
+        expectedResponse
+      );
+
+      await expect(axios.get(`/api/pokemons?limit=NaN`)).rejects.toThrow(
+        expectedResponse
+      );
+    });
+
+    it('scenario6: should return expected result when invalid cursor params init', async () => {
+      // Given
+      const expectedResponse = 'Request failed with status code 400';
+
+      // When & Then
+      await expect(axios.get(`/api/pokemons?cursor=2`)).rejects.toThrow(
+        expectedResponse
+      );
+
+      await expect(axios.get(`/api/pokemons?lastId=2`)).rejects.toThrow(
+        expectedResponse
+      );
+    });
+
+    it('scenario7: should return expected result when invalid query params init', async () => {
+      // Given
+      const expectedResponse = expectedObjectScenario7;
+      const params = qs.stringify({
+        name: '',
+        types: 'aa',
+        color: 'aa',
+        shape: 'aa',
+      });
+
+      try {
+        // When
+        await axios.get(`/api/pokemons?${params}`);
+      } catch (error) {
+        // Then
+        expect(error.response.status).toBe(400);
+
+        expect(error.response.data).toMatchObject(expectedResponse);
+      }
+    });
+
+    it('scenario8: should return expected result when unwhitelist params init', async () => {
+      // Given
+      const expectedResponse = 'Request failed with status code 400';
+
+      // When & Then
+      await expect(axios.get(`/api/pokemons?unlisted=true`)).rejects.toThrow(
+        expectedResponse
+      );
     });
   });
 });
