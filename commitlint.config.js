f4681863eddd2ed0bbdd6770e82a019bd1c95e78
munirapp <softwaremakassar@gmail.com>
2024-10-13 18:47:23 +0700

    chore: increase body max line length commit message

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,6 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [2, 'always', 200], // Increase the body line length to 200
+  },
+};
