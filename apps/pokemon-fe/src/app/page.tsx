908f52e32ac32f5138723f997502a2ba44a6cc7b
munirapp <softwaremakassar@gmail.com>
2024-10-31 08:42:44 +0700

    feat: init page.tsx

--- apps/pokemon-fe/src/app/page.tsx
+++ apps/pokemon-fe/src/app/page.tsx
@@ -1,12 +1,12 @@
+'use client';
+
+import { Navbar } from '../components/Navbar';
+import './page.scss';
+
 export default function Index() {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.scss file.
-   */
   return (
-    <div>
-      <h1>Whereas disregard and contempt for human rights have resulted</h1>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   );
 }
