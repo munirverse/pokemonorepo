91f98107983b1621793c163134ee50f717d8a5bc
munirapp <softwaremakassar@gmail.com>
2024-11-02 19:41:59 +0700

    fix: implement search state logic in navbar component

--- apps/pokemon-fe/src/components/Navbar.tsx
+++ apps/pokemon-fe/src/components/Navbar.tsx
@@ -1,12 +1,45 @@
 'use client';
 
-import { useMediaQuery } from '@mantine/hooks';
+import { useDebouncedCallback, useMediaQuery } from '@mantine/hooks';
 
 import { DekstopNavbar } from './DesktopNavbar';
 import { MobileNavbar } from './MobileNavbar';
+import {
+  useSearchDispatch,
+  useSearchSelector,
+} from '../lib/features/search/searchHook';
 
 export const Navbar = () => {
+  // selector
   const isMobile = useMediaQuery('(max-width: 890px)');
 
-  return isMobile ? <MobileNavbar /> : <DekstopNavbar />;
+  const search = useSearchSelector();
+
+  // dispatch
+  const searchDispatch = useSearchDispatch();
+
+  // handler
+  const handleSearchBehaviour = useDebouncedCallback(
+    async (queryText: string) => {
+      searchDispatch.activateSearchMode();
+    },
+    700
+  );
+
+  const handleSetQueryText = (e: React.ChangeEvent<HTMLInputElement>) => {
+    searchDispatch.setQueryText(e.target.value);
+    handleSearchBehaviour(e.target.value);
+  };
+
+  return isMobile ? (
+    <MobileNavbar
+      queryText={search.queryText}
+      onSetQueryText={handleSetQueryText}
+    />
+  ) : (
+    <DekstopNavbar
+      queryText={search.queryText}
+      onSetQueryText={handleSetQueryText}
+    />
+  );
 };
