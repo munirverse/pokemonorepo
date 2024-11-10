db38004f1098741ce6a28a8bfbe0cd790a80ebb4
munirapp <softwaremakassar@gmail.com>
2024-11-10 12:12:40 +0700

    fix: add transform response in getPokemon endpoints

--- apps/pokemon-fe/src/lib/features/search/searchApi.ts
+++ apps/pokemon-fe/src/lib/features/search/searchApi.ts
@@ -1,17 +1,18 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { GetPokemonRes } from './searchType';
+import { GetPokemonRes, PokemonBasic } from './searchType';
 
 export const pokemonApi = createApi({
   reducerPath: 'pokemonApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5300/api' }),
   endpoints: (builder) => ({
-    getPokemon: builder.query<GetPokemonRes, string>({
+    getPokemon: builder.query<PokemonBasic[], string>({
       query: (params) => {
         return {
           url: `pokemons?${params}`,
           method: 'GET',
         };
       },
+      transformResponse: (response: GetPokemonRes) => response.data,
     }),
   }),
 });
