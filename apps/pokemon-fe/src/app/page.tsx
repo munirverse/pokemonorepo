dd59e7879de10860057deab4394d6fa80a09d08f
munirapp <softwaremakassar@gmail.com>
2024-11-10 22:19:52 +0700

    fix(pokemon-fe): init base get pokemon query

--- apps/pokemon-fe/src/app/page.tsx
+++ apps/pokemon-fe/src/app/page.tsx
@@ -4,26 +4,44 @@ import qs from 'querystring';
 import { Navbar } from '../components/Navbar';
 import './page.scss';
 import {
-  useSearchSelector,
   useGetPokemonQuery,
+  useSearchSelector,
 } from '../lib/features/search/searchHook';
+import { GetPokemonPayload } from '../lib/features/search/searchType';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Index() {
   // selector
   const search = useSearchSelector();
 
+  const router = useRouter();
+
   // query api
+  const baseGetPokemonqQuery: GetPokemonPayload = {
+    page: 1,
+    limit: 10,
+  };
+
   const {
     data: pokemons,
     error: getPokemonError,
     isLoading: isGetPokemonLoading,
-  } = useGetPokemonQuery(qs.stringify({ name: search.queryParams }), {
-    skip: !search.queryParams,
-  });
+    isSuccess: isGetPokemonSuccess,
+  } = useGetPokemonQuery(qs.stringify(baseGetPokemonqQuery));
+
+  // use effect
+  useEffect(() => {
+    if (search.queryParams) {
+      router.push(`/search?q=${search.queryParams}`);
+    }
+  }, [search]);
 
   return (
     <main>
       <Navbar />
+      {isGetPokemonLoading && <pre>Loading...</pre>}
+      {isGetPokemonSuccess && <pre>{JSON.stringify(pokemons)}</pre>}
     </main>
   );
 }
