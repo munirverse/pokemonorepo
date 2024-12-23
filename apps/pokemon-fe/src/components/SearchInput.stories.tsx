98240a5c52dec05841087ccb512c2521af29fc66
munirapp <softwaremakassar@gmail.com>
2024-12-23 22:47:04 +0700

    fix(pokemon-fe): improve search input stories

--- apps/pokemon-fe/src/components/SearchInput.stories.tsx
+++ apps/pokemon-fe/src/components/SearchInput.stories.tsx
@@ -1,59 +1,54 @@
+import { useState } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
-import { MantineProvider, List, Text } from '@mantine/core';
-import '@mantine/core/styles.css';
-
-import { StoreProvider } from '../app/StoreProvider';
+import { action } from '@storybook/addon-actions';
+import { MantineProvider } from '@mantine/core';
 import { SearchInput, SearchInputProps } from './SearchInput';
-import { useSearchSelector } from '../lib/features/search/searchHook';
+import '@mantine/core/styles.css';
 
 const meta = {
   title: 'Search Input',
   component: SearchInput,
+  argTypes: { size: { options: ['xs', 'sm', 'md', 'lg', 'xl'] } },
   parameters: {
     layout: 'centered',
+    argTypesRegex: '^on.*',
   },
 } satisfies Meta<typeof SearchInput>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-const SearchInputGroups = (props: SearchInputProps) => {
-  // selector
-  const search = useSearchSelector();
+const SearchInputWrapper = (props: SearchInputProps) => {
+  const [queryText, setQueryText] = useState<string>(props.value);
+
+  const onChange = (value: string) => {
+    action('onChange')(value);
+    setQueryText(value);
+  };
+
+  const onKeydown = (value: string) => {
+    action('onKeydown')(value);
+  };
 
   return (
-    <List spacing={'md'} listStyleType={'none'}>
-      <List.Item>
-        {' '}
-        <SearchInput {...props} />
-      </List.Item>
-      <List.Item>
-        {' '}
-        <Text size="xs">Search Query Text: {search.queryText}</Text>
-      </List.Item>
-      <List.Item>
-        {' '}
-        <Text size="xs">Search Query Params: {search.queryParams}</Text>
-      </List.Item>
-      <List.Item>
-        {' '}
-        <Text size="xs">
-          Search Active Status: {search.active ? 'true' : 'false'}
-        </Text>
-      </List.Item>
-    </List>
+    <SearchInput
+      {...props}
+      value={queryText}
+      onChange={onChange}
+      onKeydown={onKeydown}
+    />
   );
 };
 
 export const Default: Story = {
   args: {
-    placeholder: 'Search Pokemon',
+    placeholder: "Search Pokemon's name",
+    size: "xl",
+    value: 'Pikachu',
   },
   render: (props) => (
     <MantineProvider>
-      <StoreProvider>
-        <SearchInputGroups {...props} />
-      </StoreProvider>
+      <SearchInputWrapper {...props} />
     </MantineProvider>
   ),
 };
