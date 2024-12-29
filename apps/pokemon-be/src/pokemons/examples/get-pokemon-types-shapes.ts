035d105a916d9131bc1b85e95eed3a670c407abc
munirapp <softwaremakassar@gmail.com>
2024-12-29 09:31:56 +0700

    fix(pokemon-be): add get types and shapes on api specs

---
+++ apps/pokemon-be/src/pokemons/examples/get-pokemon-types-shapes.ts
@@ -0,0 +1,50 @@
+export default {
+  status: 200,
+  description: 'Success Get List of Pokemon Types',
+  example: {
+    data: [
+      {
+        shape: 'tentacles',
+      },
+      {
+        shape: 'arms',
+      },
+      {
+        shape: 'blob',
+      },
+      {
+        shape: 'heads',
+      },
+      {
+        shape: 'quadruped',
+      },
+      {
+        shape: 'upright',
+      },
+      {
+        shape: 'armor',
+      },
+      {
+        shape: 'legs',
+      },
+      {
+        shape: 'fish',
+      },
+      {
+        shape: 'squiggle',
+      },
+      {
+        shape: 'ball',
+      },
+      {
+        shape: 'bug-wings',
+      },
+      {
+        shape: 'wings',
+      },
+      {
+        shape: 'humanoid',
+      },
+    ],
+  },
+};
--- apps/pokemon-be/src/pokemons/pokemons.controller.ts
+++ apps/pokemon-be/src/pokemons/pokemons.controller.ts
@@ -2,7 +2,9 @@ import { Controller, Get, Query } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { PokemonsService } from './pokemons.service';
 import { FindPokemonDto } from './dto/find-pokemon.dto';
-import findPokemonExample from './examples/find-pokemon.example';
+import findPokemonsExample from './examples/find-pokemons.example';
+import getPokemonTypesListExample from './examples/get-pokemon-types-list';
+import getPokemonTypesShapesExample from './examples/get-pokemon-types-shapes';
 
 @ApiTags('pokemons')
 @Controller('pokemons')
@@ -11,19 +13,21 @@ export class PokemonsController {
 
   @Get()
   @ApiOperation({ summary: 'Find Pokemons' })
-  @ApiResponse(findPokemonExample)
+  @ApiResponse(findPokemonsExample)
   async findPokemons(@Query() pokemonDto: FindPokemonDto) {
     return this.service.findPokemons(pokemonDto);
   }
 
   @Get('/types')
   @ApiOperation({ summary: 'Get List of Pokemon Types' })
+  @ApiResponse(getPokemonTypesListExample)
   async getPokemonTypesList() {
     return this.service.getPokemonTypesList();
   }
 
   @Get('/shapes')
   @ApiOperation({ summary: 'Get List of Pokemon Shapes' })
+  @ApiResponse(getPokemonTypesShapesExample)
   async getPokemonShapesList() {
     return this.service.getPokemonShapesList();
   }
