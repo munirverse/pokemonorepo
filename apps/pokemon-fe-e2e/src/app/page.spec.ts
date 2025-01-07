8abc7ddd53aadf92db56b28f7e2d0474f3ddc61b
munirapp <softwaremakassar@gmail.com>
2025-01-07 19:53:05 +0700

    test(pokemon-fe): improve homepage e2e test case

--- apps/pokemon-fe-e2e/src/app/page.spec.ts
+++ apps/pokemon-fe-e2e/src/app/page.spec.ts
@@ -51,4 +51,103 @@ test.describe('Homepage E2E Tests', () => {
 
     expect(page.url()).toBe('http://localhost:3000/search?q=pikachu');
   });
+
+  test('should request certain response when filter and pagination was selected', async ({
+    page,
+  }) => {
+    // go to FE url
+    await page.goto('http://localhost:3000/');
+
+    // init promise api response function
+    const resPromise = (urlTarget: string) =>
+      page.waitForResponse(async (res) => {
+        const isTargetedEndpoint = res.url().includes(urlTarget);
+
+        if (!isTargetedEndpoint) return false;
+        return res.status() === 200;
+      });
+
+    // type filter test
+    const typesFilter = page.getByRole('textbox', { name: 'Types' }).first();
+
+    await typesFilter.click();
+
+    await page.getByRole('option', { name: 'Fire' }).click();
+
+    const typeFilterResponse = await resPromise('&types=');
+
+    const typeFilterResponseBody = await typeFilterResponse.json();
+
+    await expect(typesFilter).toHaveValue('Fire');
+
+    expect(typeFilterResponse.status()).toBe(200);
+
+    expect(typeFilterResponseBody.pagination).toBeDefined();
+
+    expect(typeFilterResponseBody.data).toBeDefined();
+
+    expect(typeFilterResponseBody.data.length).toBeGreaterThan(0);
+
+    expect(typeFilterResponseBody.data[0].types).toContain('fire');
+
+    // shape filter test
+    const shapeFilter = page.getByRole('textbox', { name: 'Shape' }).first();
+
+    await shapeFilter.click();
+
+    await page.getByRole('option', { name: 'Quadruped' }).click();
+
+    const shapeFilterResponse = await resPromise('&shape=');
+
+    const shapeFilterResponseBody = await shapeFilterResponse.json();
+
+    await expect(shapeFilter).toHaveValue('Quadruped');
+
+    expect(shapeFilterResponse.status()).toBe(200);
+
+    expect(shapeFilterResponseBody.pagination).toBeDefined();
+
+    expect(shapeFilterResponseBody.data).toBeDefined();
+
+    expect(shapeFilterResponseBody.data.length).toBeGreaterThan(0);
+
+    expect(shapeFilterResponseBody.data[0].shape).toBe('quadruped');
+
+    // clear filter types and shape
+    const typesFilterClear = typesFilter.locator('..').locator('button');
+    await typesFilterClear.click();
+
+    const shapeFilterClear = shapeFilter.locator('..').locator('button');
+    await shapeFilterClear.click();
+
+    // page size and page number test
+    const pageSizeFilter = page
+      .getByRole('textbox', { name: 'Page Size Desktop' })
+      .first();
+
+    await pageSizeFilter.click();
+
+    await page.getByRole('option', { name: '16' }).click();
+
+    const pageNumberButton = page
+      .locator('.mantine-Pagination-root')
+      .locator('button')
+      .nth(2);
+
+    await pageNumberButton.click();
+
+    const paginationResponse = await resPromise('page=2&limit=16');
+
+    const paginationResponseBody = await paginationResponse.json();
+
+    expect(paginationResponse.status()).toBe(200);
+
+    expect(paginationResponseBody.pagination).toBeDefined();
+
+    expect(paginationResponseBody.data).toBeDefined();
+
+    expect(paginationResponseBody.pagination.pageNumber).toBe(2);
+
+    expect(paginationResponseBody.pagination.pageSize).toBe(16);
+  });
 });
